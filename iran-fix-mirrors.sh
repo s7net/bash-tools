776c7fd3bae9bc32f94cab20a4ca37bd68b6abd2
- 𝙰𝚖𝚒𝚛𝙷𝚘𝚜𝚜𝚎𝚒𝚗 [刃] <148908703+s7net@users.noreply.github.com>
2025-06-18 02:56:33 +0330

    Update iran-fix-mirrors.sh

--- iran-fix-mirrors.sh
+++ iran-fix-mirrors.sh
@@ -4,6 +4,6 @@ sed -i 's|http://deb.debian.org/debian|http://mirror.arvancloud.ir/debian|g' /et
 apt update
 rm -f /etc/resolv.conf
 touch /etc/resolv.conf
-echo 'nameserver 10.202.10.202' >> /etc/resolv.conf
-echo 'nameserver 10.202.10.102' >> /etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+echo 'nameserver 1.1.1.1' >> /etc/resolv.conf
 echo "185.199.108.133 raw.githubusercontent.com" > /etc/hosts
