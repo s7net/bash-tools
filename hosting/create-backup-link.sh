34e29a038a8e760a14a4b16807ff64abbbc11dc9
- 𝙰𝚖𝚒𝚛𝙷𝚘𝚜𝚜𝚎𝚒𝚗 [刃] <148908703+s7net@users.noreply.github.com>
2025-07-10 21:40:59 +0330

    Update create-backup-link.sh

--- hosting/create-backup-link.sh
+++ hosting/create-backup-link.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 clear
-
 read -p "Enter server folder (e.g. lh420): " s
 read -p "Enter backup username (e.g. firfirir): " u
+mapfile -t files < <(find /home/"$s"/ \( -path "/home/$s/weekly*" -o -path "/home/$s" \) -type f -name "*$u*" 2>/dev/null)
 
-mapfile -t files < <(find /home/$s/weekly*/ -type f -name "*$u*" 2>/dev/null)
-
-[ ${#files[@]} -eq 0 ] && echo "❌ No backup files found." && exit 1
+if [ ${#files[@]} -eq 0 ]; then
+  echo "❌ No backup files found."
+  exit 0
+fi
 
 echo "Available backups:"
 for i in "${!files[@]}"; do
@@ -19,16 +19,16 @@ done
 
 read -p "#? " n
 
-[[ $n =~ ^[0-9]+$ ]] && [ $n -ge 1 ] && [ $n -le ${#files[@]} ] || {
+if ! [[ $n =~ ^[0-9]+$ ]] || [ "$n" -lt 1 ] || [ "$n" -gt "${#files[@]}" ]; then
   echo "❌ Invalid selection"
-  exit 1
-}
+  exit 0
+fi
 
 sel="${files[$((n-1))]}"
 
-cp "$sel" /var/www/html/
-chmod 644 /var/www/html/$(basename "$sel")
-chown root:root /var/www/html/$(basename "$sel")
+cp "$sel" /var/www/html/ &&
+chmod 644 /var/www/html/"$(basename "$sel")" &&
+chown root:root /var/www/html/"$(basename "$sel")"
 
 ip=$(hostname -I | awk '{print $1}')
 echo "✅ File ready at: http://$ip/$(basename "$sel")"
